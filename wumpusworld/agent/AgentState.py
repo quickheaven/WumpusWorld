5cedf7637b7006f5a5043423ac97555eefa8c5d2
arjie cristobal <ajie.io@yahoo.com>
2024-02-07 23:12:46 -0500

    Continue the development of MovePlanningAgent next_action.

--- wumpusworld/agent/AgentState.py
+++ wumpusworld/agent/AgentState.py
@@ -1,3 +1,4 @@
+from wumpusworld.agent.Orientation import Orientation
 from wumpusworld.agent.orientation.Coords import Coords
 from wumpusworld.agent.orientation.East import East
 from wumpusworld.agent.orientation.North import North
@@ -10,7 +11,7 @@ class AgentState:
 
     def __init__(self):
         self._location = Coords(0, 0)
-        self._orientation = East
+        self._orientation = East()
         self._has_gold = False
         self._has_arrow = False
         self._is_alive = True
@@ -19,25 +20,33 @@ class AgentState:
     def has_gold(self) -> bool:
         return self._has_gold
 
+    @has_gold.setter
+    def has_gold(self, has_gold: bool) -> None:
+        self._has_gold = has_gold
+
     @property
     def has_arrow(self) -> bool:
         return self._has_arrow
+
+    @has_arrow.setter
+    def has_arrow(self, has_arrow: bool) -> None:
+        self._has_arrow = has_arrow
 
     @property
     def is_alive(self) -> bool:
         return self._is_alive
 
-    @property
-    def location(self) -> Coords:
-        return self._location
+    @is_alive.setter
+    def is_alive(self, is_alive: bool) -> None:
+        self._is_alive = is_alive
 
     def turn_left(self):
-        self._orientation = self._orientation.turn_left
+        self._orientation = self._orientation.turn_left()
 
     def turn_right(self):
-        self._orientation = self._orientation.turn_right
+        self._orientation = self._orientation.turn_right()
 
-    def forward(self, grid_width: int, grid_height: int):
+    def forward(self, grid_width, grid_height):
         new_agent_location = None
         if isinstance(self._orientation, West):
             new_agent_location = Coords(self._location.x, max(0, self._location.y - 1))
@@ -55,8 +64,16 @@ class AgentState:
 
         return new_agent_location
 
+    @property
+    def orientation(self) -> Orientation:
+        return self._orientation
+
+    @property
+    def location(self) -> Coords:
+        return self._location
+
     def use_arrow(self):
-        self._hasArrow = False
+        self._has_arrow = False
 
     def apply_move_action(self, action: Action, grid_width: int, grid_height: int):
         action_id: int = Action.get_by_value(str(action))
@@ -67,3 +84,7 @@ class AgentState:
                 self.turn_left()
             case 2:  # TURN RIGHT
                 self.turn_right()
+
+    def __str__(self):
+        return ('location: {}, orientation: {}, has_gold: {}, has_arrow: {}, is_alive: {}'
+                .format(self._location, self._orientation, self._has_gold, self._has_arrow, self._is_alive))
--- wumpusworld/agent/MovePlanningAgent.py
+++ wumpusworld/agent/MovePlanningAgent.py
@@ -14,7 +14,7 @@ class MovePlanningAgent(Agent):
         self._name = "MOVE_PLANNING_AGENT"
         self._grid_width = grid_width
         self._grid_height = grid_height
-        self._agent_state = AgentState
+        self._agent_state = AgentState()
         self._safe_locations = set()
         self._action_list = []
 
@@ -25,34 +25,65 @@ class MovePlanningAgent(Agent):
         return '{} ({})'.format(self._name, is_alive_str)
 
     def next_action(self, percept: Percept) -> Action:
+        action_int = -1
+        # Update the state of the Move Planning agent before running the next_action on the environment level.
+        # Inspiration comes from Scala project branch beeline-agent
 
         if self._agent_state.has_gold:
+            print('The agent have the gold. Performing an escape plan.')
             if self._agent_state.location == Coords(0, 0):
-                random_action_int = 4  # Climb
-                return Action.get_by_id(random_action_int)
+                action_int = 4  # Climb
+
             else:
                 # add action to the list.
                 indexes = [i for i in range(2)]  # Exclude Grab (3) Climb (4).
-                beeline_action_int: int = choice(indexes)
-                beeline_action: Action = Action.get_by_id(beeline_action_int)
-                # TODO move back to original location based on shortest path
-                #
+                action_int = choice(indexes)
+                """
+                TODO move back to original location based on shortest path
+                Study further networkx.
+                Plan the graph and edges.
+                Find the shortest path based on the Coords.
+                Translate the shortest path into list of actions.
+                Used that list of actions to go back to starting cell.
+                """
+                if len(self._action_list) == 0:
+                    # call a function that will use networkx to build the shortest path.
+                    pass
+                else:
+                    # execute an action plan based on action_list.
+                    pass
+
                 # grid_width: int = self._grid_width
                 # grid_height: int = self._grid_height
-                #
+
                 # self._agent_state = self._agent_state.apply_move_action(beeline_action, grid_width, grid_height)
-                # self._action_list = self._action_list.append(beeline_action_int)
+                # self._action_list = self._action_list.append(action_int)
 
-                return beeline_action
+        elif percept.glitter():
+            self._agent_state.has_gold = True
+            action_int = 3  # Grab
 
-        elif percept.glitter:
-            self.has_gold = True
-            random_action_int = 3  # Grab
-            return Action.get_by_id(random_action_int)
         else:
             indexes = [i for i in range(6) if i not in [3, 4]]  # Exclude Grab (3) Climb (4).
-            random_action_int: int = choice(indexes)
-            return Action.get_by_id(random_action_int)
+            action_int = choice(indexes)
+
+            match action_int:
+                case 0:
+                    new_safe_location = self._agent_state.forward(self._grid_width, self._grid_height)
+                    self._safe_locations.add(new_safe_location)
+                case 1:
+                    self._agent_state.turn_left()
+                case 2:
+                    self._agent_state.turn_right()
+                case 2:
+                    self._agent_state.use_arrow = True
+
+        safe_locations = [str(coords) for coords in self._safe_locations]
+        action_list = [str(i) for i in self._action_list]
+        print('MovePlanningAgent.next_action grid_width: {}, grid_height: {}, \nagent_state: {}, \nsafe_locations: {}, '
+              '\naction_list: {}'
+              .format(self._grid_width, self._grid_height, self._agent_state, safe_locations, action_list))
+        return Action.get_by_id(action_int)
 
     def to_string(self) -> str:
         parent_class_str = super().__str__()
